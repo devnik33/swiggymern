6b1bba2736e681c83187cb6d296663606f5f7e1c
devniki33 <nikitakhatri33@gmail.com>
2024-06-25 17:59:28 +0530

    first file

---
+++ arithmetic.js
@@ -0,0 +1,65 @@
+// let x=12;
+// let y=3;
+
+let x="12";
+let y="3";
+
+//let x="hello";
+//let y="my";
+
+
+
+console.log(x+y,'addition');  //this  value concatenate this
+console.log(x-y,'subtraction');  //this give the special value NaN (not a number)
+console.log(x*y,'multiplication');
+console.log(x/y,'division');
+console.log(x**2,'x power of 2');
+console.log(x**3,'y power of 3');
+console.log(x**4,'y power of 4');
+
+console.log(Boolean("")); //empty boolean value give the false 
+
+let xb=-1;
+
+console.log(Boolean(xb));
+//if have any boolean value give the true 
+//if boolean value is zero give the false and all other are true
+
+console.log(-(4 ** 2));
+
+console.log(2 ** 2 ** 2)
+
+// var a=2;
+// b=++a;
+
+// console.log(b);
+
+// var at=2;
+// bt=at++;
+
+// console.log(bt);
+
+// var as=5;
+// bs=--as;
+// console.log(bs);
+
+// var asa=5;
+// bsa=asa--;
+// console.log(asa);
+
+// var a=1;
+// b=--a;
+
+
+// console.log(b);
+
+var a=5;
+b=a--;
+
+console.log(a);
+console.log("hello");
+console.log(+4);
+console.log(+"2");
+console.log(+true);
+console.log(+false);
+console.log(+null);
