7937ecedde9d3f4f6d1ba6440cca7cc8965b7089
devniki33 <nikitakhatri33@gmail.com>
2024-06-25 17:46:32 +0530

    first file

---
+++ ananomysfun.js
@@ -0,0 +1,16 @@
+
+var hello=function(){
+    console.log("hello");
+}
+
+hello();
+
+
+console.log(typeof hello);
+let num=-12;
+
+console.log(Math.sign(num))
+
+
+// 1.What is ananonyms function ?
+// ans => ananonyms function is function that does not have name associated with that normally we use the function name without using the to define a function in javascript.we use only the function keyword without using the function name  
